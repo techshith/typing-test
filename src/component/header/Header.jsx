3c1ff61894a96e24fea7140af244ee7747e33ae6
Veekshith Kajeepeta <veekshith.k@gmail.com>
2025-03-24 03:30:53 -0500

    add header

---
+++ src/component/header/Header.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import './header.css'
+
+function Header() {
+  return (
+    <div className="header">
+      <h2 className='headerInfo'>Typing Test</h2>
+    </div>
+  )
+}
+
+export default Header
